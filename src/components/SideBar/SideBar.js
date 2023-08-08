e64af9582c570a2279c5d413501a8dc8238dc139
anju <anjualfino@gmail.com>
2023-08-08 18:09:04 +0800

    sidebar bug fixed

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,73 +1,19 @@
-/*  import React, { useState } from "react";
-import styled from 'styled-components';
-import { Link } from "react-router-dom";
-import './SideBar.css'; // Import the CSS file
-
-function Sidebar() {
-   
-    return(
-       <div className="SidebarContainer">
-           <div className="right-items">
-               <div className="SidebarItem">
-                   <Link to={{ pathname: "/aboutme" }}>About Me</Link>
-               </div>
-               <div className="SidebarItem">
-                   <Link to={{ pathname: "/recentworks" }}>Recent Works</Link>
-               </div>
-               <div className="SidebarItem">
-                   <Link to={{ pathname: "/resume" }}>Education</Link>
-               </div>
-               <div className="SidebarItem">
-                   <Link to={{ pathname: "/resume" }}>Experience</Link>
-               </div>
-               <div className="SidebarItem">
-                   <Link to={{ pathname: "/resume" }}>Resume</Link>
-               </div>
-               <div className="SidebarItem">
-                   <Link to={{ pathname: "/contact" }}>Contact</Link>
-               </div>
-                 
-           </div>
-       </div>
-   ); 
-   
-}
-
-export default Sidebar;
- */
 
 import React, { useState } from "react";
-import styled from 'styled-components';
 import './SideBar.css'; // Import the CSS file
 
 function Sidebar() {
-   
-    return(
-       <div className="SidebarContainer">
-           <div className="right-items">
-               <div className="SidebarItem">
-               <a href="#AboutMeId">About Me</a>
-               </div>
-               <div className="SidebarItem">
-               <a href="#PortFolioId">Recent Works</a>
-               </div>
-               <div className="SidebarItem">
-               <a href="#ExperienceId" >Experience</a>
-               </div>
-               <div className="SidebarItem">
-               <a href="#EducationId" >Education</a>
-               </div>
-               <div className="SidebarItem">
-               <a href="#ResumeId" >Resume</a>
-               </div>
-               <div className="SidebarItem">
-               <a href="#ContactId" >Contact</a>
-               </div>
-                 
-           </div>
-       </div>
-   ); 
-   
+
+    return (
+        <div className="SidebarContainer">
+            <a href="#AboutMeId">About Me</a>
+            <a href="#PortFolioId">Recent Works</a>
+            <a href="#ExperienceId" >Experience</a>
+            <a href="#EducationId" >Education</a>
+            <a href="#ResumeId" >Resume</a>
+            {/* <a href="#ContactId" >Contact</a> */}
+        </div>
+    );
 }
 
 export default Sidebar; 
